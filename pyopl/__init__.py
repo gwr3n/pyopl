be5abf4ae0e22fcaedaddd0b3ebcfab344398dac
Roberto Rossi <robros@gmail.com>
2026-06-29 17:11:36 +0100

    Bump version to 1.8.5 in __init__.py and pyproject.toml

--- pyopl/__init__.py
+++ pyopl/__init__.py
@@ -7,7 +7,7 @@ import sys
 from .genai.pyopl_generative import generative_feedback, generative_solve
 from .pyopl_core import solve
 
-__version__ = "1.8.4"
+__version__ = "1.8.5"
 __year__ = "2026"
 __author__ = "Roberto Rossi"
 __email__ = "robros@gmail.com"
